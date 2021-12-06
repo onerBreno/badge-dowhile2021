85f3b8cd91787eb8724516ee51c5ecf588ff8d86
onerbreno <onerbreno@outlook.com>
2021-12-06 10:32:04 -0300

    Mobile badge DoWhile21

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  youtube: 'channel/UC5WZfGUSt6EEmC5ar1-AiOw',
+  instagram: 'jakeliny.gracielly',
+  facebook: 'onerbreno',
+  twitter: 'onerbreno',
+  github: 'onerbreno'
+}
+
+function changeLinkSocialMedia() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeLinkSocialMedia()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = `${data.html_url}`
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
